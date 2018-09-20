7792ccd2db16ca04eefa69fde9f9aca879a49945
Michael <mikeishere3@gmail.com>
2018-09-19 22:35:41 -0400

    created validation and web token generation

--- devconnector/config/keys.js
+++ devconnector/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://mike:michael1@ds163382.mlab.com:63382/my-devconnector'
+  mongoURI: 'mongodb://mike:michael1@ds163382.mlab.com:63382/my-devconnector',
+  secretOrKey: 'secret'
 }
