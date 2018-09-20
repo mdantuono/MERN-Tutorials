e9675608f33606490cba12356e954e738745ad95
Michael <mikeishere3@gmail.com>
2018-09-20 14:03:33 -0400

    registration and login validation completed

--- devconnector/routes/api/users.js
+++ devconnector/routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load input validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load user model
 const User = require('../../models/User');
@@ -23,10 +24,18 @@ router.get('/test', (req, res) => {
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email })
     .then(user => {
         if (user) {
-          return res.status(400).json({ email: 'Email already exists'});
+          errors.email = 'Email already exists';
+          return res.status(400).json(errors);
         } else {
           const avatar = gravatar.url(req.body.email, {
             s: '200', // size
@@ -58,15 +67,24 @@ router.post('/register', (req, res) => {
 // @desc    Log user in / Return JWT token
 // @access  Public
 router.post('/login', (req, res) => {
+
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   // Find user by email
   User.findOne({email})
     .then(user => {
-      // Check for users
+      // Check for user
       if (!user) {
-        return res.status(404).json({email: 'User not found'});
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
       }
       // Check password
       bcrypt.compare(password, user.password)
@@ -82,7 +100,8 @@ router.post('/login', (req, res) => {
               });
             }); // Expires after one hour
           } else {
-            return res.status(400).json({password: 'Password incorrect'});
+            errors.password = 'Password incorrect'
+            return res.status(400).json(errors);
           }
         })
     })
--- devconnector/validation/register.js
+++ devconnector/validation/register.js
@@ -1,15 +1,49 @@
 const Validator = require('validator');
-import isEmpty from './is-empty';
+const isEmpty = require('./is-empty');
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
+  data.name = !isEmpty(data.name) ? data.name : '';
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+  data.password2 = !isEmpty(data.password2) ? data.password2 : '';
+
+  // Data validation
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = 'Name must be between 2 and 30 characters';
+  }
+  // Check if name field is empty
+  if (Validator.isEmpty(data.name)) {
+    errors.name = 'Name field is required';
+  }
+  // Check if email field is empty
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+  // Check if email is valid
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+  // Check if password is empty
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required"
+  }
+  // Check if password is of correct length
+  if (!Validator.isLength(data.password, {min: 6, max: 30})) {
+    errors.password = "Password must be at least 6 characters"
+  }
+  // Check if confirm password field is empty
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required"
+  }
+  // Confirm password field matches password
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match"
   }
 
   return {
     errors,
-    isValid: sEmpty(errors)
+    isValid: isEmpty(errors)
   }
 }
